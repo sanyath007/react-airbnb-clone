0eb1fdabdefd18f3322e48c0acf3399956b837cf
Sanyath Thammawong <sanyath007@gmail.com>
2020-09-07 06:43:12 +0700

    Build Header and Home components and completly style Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import './App.css';
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Let's build the AirBNB clone 🚀 </h1>
+      {/* Header */}
+      <Header />
+      
+      {/* Home Page */}
+      <Home />
+
+
+        {/* Banner */}
+
+        {/* Cards */}
+
+        {/* Footer */}
+
+      {/* Search Page */}
+
+        {/* ... */}
     </div>
   );
 }
