7e3d500c174283ceec090a4de0fa92b318b9188c
Sanyath Thammawong <sanyath007@gmail.com>
2020-09-10 19:07:51 +0700

    Build SearchPage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import SearchPage from './components/SearchPage/SearchPage';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App">
-      {/* Header */}
-      <Header />
-      
-      {/* Home Page */}
-      <Home />
-
-        {/* Banner */}
-
-        {/* Cards */}
-
-        {/* Footer */}
-        <Footer />
+      <Router>
+        <Header />
         
-      {/* Search Page */}
+        <Switch>
+          <Route path="/search">
+            <SearchPage />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-        {/* ... */}
+        <Footer />
+      </Router>
     </div>
   );
 }
